979ac81334f0b9a6dce6e80b6c793b00a41c96f0
teachAlexIt <alekseiteacherit@gmail.com>
2023-04-18 13:20:02 +0300

    wrapperClick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import GroupListItem from './Components/GroupListItem';
 import NewGroupBox from './Components/NewGroupBox';
-import Store from './Store/Store';
+import Store, { ModalBlockActiveChange } from './Store/Store';
 
 function App() {
   let GroupList = Store.GroupList.map((Item) =>
@@ -10,9 +10,18 @@ function App() {
       key={Item.id}
       name={Item.name}
       status={Item.status} />)
+  function wrapperClick(e) {
+    if(e.target.classList.contains('wrapper')) {
+      ModalBlockActiveChange('', '_hide');
+    }
+    
+  }
   return (
     <div className='App'>
-    <NewGroupBox status={Store.ModalBlocks[0].status}/>
+      <div className={'wrapper ' + Store.ModalBlocks[0].status}
+        onClick={wrapperClick}>
+        <NewGroupBox status={Store.ModalBlocks[1].status} />
+      </div>
       <h1>Мои задачи</h1>
       <div className='Task-box'>
         <div className='Task-box__head Group-list'>
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,12 +2,13 @@ import { MainRnder } from "../index";
 let Store = {
   GroupList: [
     { id: 0, name: 'Все', status: 'Group-list__item_active' },
-    { id: 1, name: 'Собрание', status: '' },
-    { id: 2, name: 'Дом', status: '' },
-    { id: 3, name: 'Работа', status: '' },
+    { id: 1, name: 'Собрание', status: 'Group-list__item_NotActive' },
+    { id: 2, name: 'Дом', status: 'Group-list__item_NotActive' },
+    { id: 3, name: 'Работа', status: 'Group-list__item_NotActive' },
     { id: 4, name: '+', status: 'New-group' }
   ],
   ModalBlocks: [
+    {name: 'wrapper', status: '_hide'},
     {name: 'NewGroup-Box', status: '_hide'},
   ]
 }
@@ -19,18 +20,26 @@ export function GroupItemActiveChange(itemID) {
     }else if(Store.GroupList[i].status === 'New-group'){
       Store.GroupList[i].status = 'New-group';
     }else{
-      Store.GroupList[i].status = '';
+      Store.GroupList[i].status = 'Group-list__item_NotActive';
     }
   }
   MainRnder();
 }
 
 export function ModalBlockActiveChange(ModalBlockName, doStatus){
-  for (let i = 0; i < Store.ModalBlocks.length; i++) {
-    if (Store.ModalBlocks[i].name === ModalBlockName){
+  Store.ModalBlocks[0].status = doStatus;
+  if (doStatus === '_show'){
+    for (let i = 0; i < Store.ModalBlocks.length; i++) {
+      if (Store.ModalBlocks[i].name === ModalBlockName){
+        Store.ModalBlocks[i].status = doStatus;
+      }
+    }
+  }else{
+    for (let i = 0; i < Store.ModalBlocks.length; i++) {
       Store.ModalBlocks[i].status = doStatus;
     }
   }
+  
   MainRnder();
 }
 export default Store;
